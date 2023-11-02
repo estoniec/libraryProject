91aa038fd0c829050cda9ff0852999860ba8234a
mmmmmmmmak <fjfjhfhf6@gmail.com>
2023-11-02 15:49:57 +0300

    update

--- rent-srvc/internal/controller/grpc/v1/mapper.go
+++ rent-srvc/internal/controller/grpc/v1/mapper.go
@@ -125,4 +125,45 @@ func NewConfirmReturnOutput(output dto.ConfirmReturnOutput) *pb.ConfirmReturnRes
 	}
 }
 
-func FindByInput(req *pb.)
+func FindByInput(req *pb.FindByRequest) dto.FindByInput {
+	return dto.FindByInput{
+		Offset: req.GetOffset(),
+		ID:     req.GetID(),
+		Time:   req.GetTime(),
+		UserID: req.GetUserID(),
+		BookID: req.GetBookID(),
+	}
+}
+
+func NewFindByOutput(output dto.FindByOutput) *pb.FindByResponse {
+	var models []*pb.BooksUsers
+	if len(output.Model) == 0 {
+		models = nil
+	} else {
+		models = make([]*pb.BooksUsers, len(output.Model))
+		for i, model := range output.Model {
+			models[i] = &pb.BooksUsers{
+				ID:       model.ID,
+				IsReturn: model.IsReturn,
+				IsGet:    model.IsGet,
+				Book: &pb.Book{
+					ID:     int64(model.Books.ID),
+					ISBN:   model.Books.ISBN,
+					Count:  int64(model.Books.Count),
+					Name:   model.Books.Name,
+					Author: model.Books.Author,
+				},
+				User: &pb.User{
+					ID:    model.Users.ID,
+					Phone: model.Users.Phone,
+					Class: model.Users.Class,
+				},
+			}
+		}
+	}
+	return &pb.FindByResponse{
+		Status: output.Status,
+		Error:  output.Error,
+		Book:   models,
+	}
+}
